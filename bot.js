21857710fbacb887d681c4a1ec2f737793793fa1
Nilyass <47905754+Nilyass@users.noreply.github.com>
2020-03-12 17:17:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
 	  console.log(`╔[ Channels [ " ${client.channels.size} " ]╗`);
 	  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
       console.log('')
-      client.user.setGame('-bc | BroadCast',`https://www.twitch.tv/BroadCast`);
+      client.user.setGame('Join = nitro',`https://www.twitch.tv/BroadCast`);
     });
 
 client.on("message", message => {
